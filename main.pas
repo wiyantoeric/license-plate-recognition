4a6b59e1d224cbd1336a58fc7495d8a590e45998
Eric Wiyanto <77096303+wiyantoeric@users.noreply.github.com>
2022-11-28 18:31:27 +0700

    initial commit

---
+++ main.pas
@@ -0,0 +1,43 @@
+unit main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
+  ExtDlgs;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    ButtonLoad: TButton;
+    ButtonExtract: TButton;
+    ImageInput: TImage;
+    LabelOutput: TLabel;
+    OpenPictureDialog1: TOpenPictureDialog;
+    procedure FormCreate(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  LabelOutput.Visible := false;
+end;
+
+end.
+
